f9fca56503d791250b377b046e2d25ae179bf819
silly <2226415191@qq.com>
2022-03-07 22:38:49 +0800

    config配置支持less

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withLess = require("next-with-less");
 
-module.exports = nextConfig
+module.exports = withLess({
+  lessLoaderOptions: {
+    lessOptions: {
+      modifyVars: {
+        // "@primary-color": "#f74a49", //自定义修改class 有primary的样式
+        // "@border-radius-base": ".5em"
+      }
+    }
+  }
+});
